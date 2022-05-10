fc1dfd79ab66026a1d9d685e1659fde1af07831c
dStojaniuk <stojaniuk.damian@gmail.com>
2022-05-10 15:50:18 +0200

    Update fields

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -44,6 +44,12 @@ const resolvers = {
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+
+    /* Track.author and Track.modules resolvers */
+    durationInSeconds: ({ length }) => length,
+  },
+  Module: {
+    durationInSeconds: ({ length }) => length,
   },
 };
 
